1a1ffd3a86d113b53b12aca439156ac827e5c6d1
Gabriel Ruiz Mussi Bersot <gabriel@debian-BULLSEYE-live-builder-AMD64>
2023-04-23 21:26:11 -0300

    Adcionado uso de métodos ao exercício 1

--- exercicios_java/EX001/src/Personagem.java
+++ exercicios_java/EX001/src/Personagem.java
@@ -3,4 +3,8 @@ public class Personagem {
     String categoria;
     int idade;
 
+    public void mostrarStatus(int lvl) {
+        System.out.printf("Boa noite %s, você é um(a) %s (lvl %d), tem %d pontos de força e %d anos de idade\n",
+                nome, categoria, lvl, lvl * idade, idade);
+    }
 }
