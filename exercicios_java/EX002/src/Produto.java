4c7c52d60107460ebef9b7e7da4c0efd6b0606a5
Gabriel Ruiz Mussi Bersot <gabriel@debian-BULLSEYE-live-builder-AMD64>
2023-04-24 15:14:15 -0300

    Adcionado exercício com uso de BigDecimal

--- exercicios_java/EX002/src/Produto.java
+++ exercicios_java/EX002/src/Produto.java
@@ -1,8 +1,14 @@
 import java.math.BigDecimal;
 
 public class Produto {
+    String categoria;
     String marca;
     String nome;
     BigDecimal preco;
     int quantidade;
+
+    void mostrarProduto() {
+        System.out.printf("%s %s da marca %s, preço: %s, possui %d unidades em estoque.\n",
+                categoria, nome, marca, preco, quantidade);
+    }
 }
